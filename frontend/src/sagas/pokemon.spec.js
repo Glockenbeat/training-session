fd033ed2c3119b958685195638eb67e1e39b686e
Peter Schramm <peter.schramm@cleverbridge.com>
2017-08-01 16:08:33 +0200

    Fixing tests, adding db to compose, fixing eslint

--- frontend/src/sagas/pokemon.spec.js
+++ frontend/src/sagas/pokemon.spec.js
@@ -9,12 +9,12 @@ describe('getPokemon', () => {
   const saga = getPokemon();
 
   it('calls axios.get with the pokeapi url', () => {
-    expect(saga.next().value).toEqual(call(axios.get, 'http://pokeapi.co/api/v2/pokemon'));
+    expect(saga.next().value).toEqual(call(axios.get, 'http://localhost:41002/pokemons'));
   });
 
   it('dispatches an action with the results list ', () => {
     const returnValue = {
-      data: ['foo', 'bar']
+      data: ['foo', 'bar'],
     };
 
     expect(saga.next(returnValue).value).toEqual(put(received(['foo', 'bar'])));
